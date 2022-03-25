5d5135012a48265b3ec0e58956c92cee6bb8c3d3
Magdalena Majta <majta.magdalena@gmail.com>
2022-03-25 22:08:24 +0100

    DB Interfaces for time

--- MoodTracker.API/Database/DataContext.cs
+++ MoodTracker.API/Database/DataContext.cs
@@ -11,4 +11,44 @@ public class DataContext : DbContext
         : base(options)
     {
     }
+
+    #region ICreatedAt SaveChanges update
+
+    private void UpdateTimestamps()
+    {
+        foreach (var entity in ChangeTracker.Entries().Where(p => p.State == EntityState.Added))
+            if (entity.Entity is ICreatedAt created)
+                created.CreatedAt = DateTime.Now;
+
+        foreach (var entity in ChangeTracker.Entries().Where(p => p.State == EntityState.Modified))
+            if (entity.Entity is IModifiedAt updated)
+                updated.ModifiedAt = DateTime.Now;
+    }
+
+    public override int SaveChanges()
+    {
+        UpdateTimestamps();
+        return base.SaveChanges();
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        UpdateTimestamps();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+        CancellationToken cancellationToken = new())
+    {
+        UpdateTimestamps();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        UpdateTimestamps();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    #endregion
 }
--- MoodTracker.API/Database/Models/User.cs
+++ MoodTracker.API/Database/Models/User.cs
@@ -1,9 +1,12 @@
 ﻿namespace MoodTracker.API.Database.Models;
 
-public class User
+public class User : ICreatedAt, IModifiedAt
 {
     public Guid Id { get; set; }
     public string UserName { get; set; }
     public string EmailAddress { get; set; }
     public string Password { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
 }
