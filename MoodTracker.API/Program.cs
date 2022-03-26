18fe1126bebc02c01ae97be3d52e4d6baee49999
Magdalena Majta <majta.magdalena@gmail.com>
2022-03-26 21:57:11 +0100

    Enable api documentation online

--- MoodTracker.API/Program.cs
+++ MoodTracker.API/Program.cs
@@ -16,14 +16,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
+    // Configure the HTTP request pipeline.
     app.UseHttpsRedirection();
 }
 
