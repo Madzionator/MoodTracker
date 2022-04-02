28fe0c35bd0af8bc64c275e6ea9bca14569984a0
Jakub <jakublula6@gmail.com>
2022-04-02 22:19:19 +0200

    Feedback on click added

--- components/RatingComp.js
+++ components/RatingComp.js
@@ -1,17 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import Emoji from './Emoji'
 import Theme from '../Theme'
+import {useState} from 'react'
 
 const RatingComp = (props) => {
+    const [pressed, setPressed] = useState()
   return (
     <View style={[styles.container, Theme.shadow]}>
         <Text style = {styles.title}>{props.title}</Text>
         <View style={styles.EmojiContainer}>
-            <Emoji code = '&#128557;' onPress = {props.onPress} id = {props.id} value = {0}/>
-            <Emoji code = '&#128533;' onPress = {props.onPress} id = {props.id} value = {1}/>
-            <Emoji code = '&#128528;' onPress = {props.onPress} id = {props.id} value = {2}/>
-            <Emoji code = '&#128522;' onPress = {props.onPress} id = {props.id} value = {3}/>
-            <Emoji code = '&#128525;' onPress = {props.onPress} id = {props.id} value = {4}/>
+            <Emoji code = '&#128557;' onPress = {props.onPress} id = {props.id} value = {0} pressed={pressed} setPressed = {setPressed}/>
+            <Emoji code = '&#128533;' onPress = {props.onPress} id = {props.id} value = {1} pressed={pressed} setPressed = {setPressed}/>
+            <Emoji code = '&#128528;' onPress = {props.onPress} id = {props.id} value = {2} pressed={pressed} setPressed = {setPressed}/>
+            <Emoji code = '&#128522;' onPress = {props.onPress} id = {props.id} value = {3} pressed={pressed} setPressed = {setPressed}/>
+            <Emoji code = '&#128525;' onPress = {props.onPress} id = {props.id} value = {4} pressed={pressed} setPressed = {setPressed}/>
         </View>
     </View>
   )
