0803852bf409115f2bad4424323968221b22aee2
Jakub <jakublula6@gmail.com>
2022-04-04 09:21:31 +0200

    Comment added

--- components/Login.js
+++ components/Login.js
@@ -23,7 +23,7 @@ export default function Login(props) {
           autoComplete = 'password'
           secureTextEntry='true'
         />
-        <Btn title = 'Zaloguj' style={styles.btn} onPress = {()=>props.setScene('Rating')}/>
+        <Btn title = 'Zaloguj' style={styles.btn} onPress = {()=>props.setScene('Rating') /*Temporary solution*/}/>
       </View>
     </LinearGradient>
   );
