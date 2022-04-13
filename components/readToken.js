617a4eb6a6deebd157c4eb1f690a50db12785e2f
Jakub <jakublula6@gmail.com>
2022-04-13 10:58:51 +0200

    Read token function in separate component with default export

--- components/readToken.js
+++ components/readToken.js
@@ -1,9 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const readToken =async()=>{
-      try {
+export default async function readToken(){ 
+  try {
         const value = await AsyncStorage.getItem('MoodTrackerToken')
         if(value !== null) {
-          console.log(value)
           return value
         }
         return null
