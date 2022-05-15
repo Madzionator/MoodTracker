6a3e5f442581e7ea0b2b321e3c38eaa7b698289a
kchrzk <102385816+kchrzk@users.noreply.github.com>
2022-05-15 10:00:07 +0200

    SearchBar
    pasek wyszukiwania

--- components/Followers.js
+++ components/Followers.js
@@ -1,13 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, Text, View } from 'react-native'
+import Btn from './Btn';
+import Theme from '../Theme'
 import React from 'react'
 const Followers = () => {
+  const [text, onChangeText] = React.useState(null);
   return (
-    <View>
-      <Text>Followers</Text>
+    <View style={styles.container}>
+      <TextInput 
+        style={styles.textInputStyle} 
+        onChangeText={onChangeText}
+        value={text} 
+        placeholder="Szukaj"> 
+      </TextInput>
+      <Btn title = 'Szukaj' style={styles.btn}>
+        <Text style = {styles.text}></Text>
+      </Btn>
     </View>
   )
 }
 
 export default Followers
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    marginTop:'10%',
+    flexDirection: 'row',
+    alignContent:'center',
+    width: '70%',
+  },
+  textInputStyle:{
+    height:66,
+    margin: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius:5,
+    fontSize:20,
+    paddingLeft: 5,
+  },
+  btn:{
+    backgroundColor:Theme.background,
+    textAlign: 'center',
+    marginTop: 5,
+    height: 66,
+    width: '55%',
+  },
+})
