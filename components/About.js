ed0a21dec0884f7e85576fdb76b0a805dc5c839f
Jakub <jakublula6@gmail.com>
2022-05-16 11:54:44 +0200

    Addicional screens added (prev removed because of git troubles)

--- components/About.js
+++ components/About.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import Btn from './Btn'
+import Theme from '../Theme'
+import { LinearGradient } from 'expo-linear-gradient'
+import Header from './Header';
 const About = (props) => {
   return (
-    <View>
-      <Text>About</Text>
-      <Btn title = 'Powrót' style={styles.btn} onPress = {()=>props.setScene('Home')}/>
-    </View>
+    <LinearGradient
+    colors={[Theme.background, Theme.backgroundGradient]}
+    style={styles.container}
+    >
+      <Header style = {{marginBottom:'15%'}}/>
+      <ScrollView>
+        <Text style={styles.text}>Copyright YEAR COPYRIGHT HOLDER
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</Text>
+        <Btn title = 'Powrót' style={styles.btn} onPress = {()=>props.setScene('Home')}/>
+      </ScrollView>
+    </LinearGradient>
   )
 }
 
 export default About
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  btn:{
+    marginHorizontal:'auto',
+    alignSelf: 'center',
+    backgroundColor:Theme.background,
+    marginBottom:'10%',
+  },
+  text:{
+    marginLeft:'5%',
+    marginRight:'5%',
+    textAlign: 'center',
+    fontSize: 30,
+    color:'white',
+    fontFamily:'ChelaOne_400Regular',
+  },
+})
--- components/Error.js
+++ components/Error.js
@@ -1,14 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
 import Btn from './Btn'
+import Theme from '../Theme'
+import { LinearGradient } from 'expo-linear-gradient'
+import Header from './Header';
 const Error = (props) => {
   return (
-    <View>
-      <Text>Error</Text>
-      <Btn title = 'Powrót' style={styles.btn} onPress = {()=>props.setScene('Home')}/>
-    </View>
+    <LinearGradient
+    colors={[Theme.background, Theme.backgroundGradient]}
+    style={styles.container}
+    >
+      <Header style = {{marginBottom:'25%'}}/>
+      <View>
+        <Text style={styles.text}> Coś poszło nie tak :/</Text>
+        <Btn title = 'Powrót' style={styles.btn} onPress = {()=>props.setScene('Home')}/>
+      </View>
+    </LinearGradient>
   )
 }
 
 export default Error
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  btn:{
+    marginHorizontal:'auto',
+    marginVertical:'auto',
+    alignSelf: 'center',
+    backgroundColor:Theme.background,
+  },
+  text:{
+    textAlign: 'center',
+    fontSize: 30,
+    color:'white',
+    fontFamily:'ChelaOne_400Regular',
+  },
+})
