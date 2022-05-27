35fb11725bde26dee2502130b8434868a5a9b22a
kchrzk <102385816+kchrzk@users.noreply.github.com>
2022-05-27 20:33:19 +0200

    ComponentWhereModalAppear

--- components/Followers.js
+++ components/Followers.js
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { useState } from "react"
 import React from 'react'
+import Accept from './Accept';
 const Followers = () => {
+  const[modalVisible, setModalVisible] = useState(true);
   return (
     <View>
+      <Accept modalVisible = {modalVisible} setModalVisible = {setModalVisible} username="Piotr"/>
       <Text>Followers</Text>
     </View>
   )
